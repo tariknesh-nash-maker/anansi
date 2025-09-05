1a544685099fb8ded7b9b09db080acf7e41b3b16
tariknesh-nash-maker <tarik.nesh-nash@opengovpartnership.org>
2025-09-05 09:46:50 +0100

    Update __init__.py

--- connectors/__init__.py
+++ connectors/__init__.py
@@ -1,1 +1,16 @@
 
+# optional: expose common fetchers here
+try:
+    from .eu_ft import fetch as fetch_eu
+except Exception:
+    fetch_eu = None
+
+try:
+    from .afdb import fetch as fetch_afdb
+except Exception:
+    fetch_afdb = None
+
+try:
+    from .afd import fetch as fetch_afd
+except Exception:
+    fetch_afd = None
